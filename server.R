e10e0e9ce0d9a4b4cc4be8989e0675b1d9e49d26
louiserfallon <louise.r.fallon@gmail.com>
2016-10-27 14:10:22 +0100

    Changed the scope of the x and error variables outside of the plot functions

--- server.R
+++ server.R
@@ -5,17 +5,14 @@
 library(shiny)
 library(ggplot2)
 shinyServer(function(input, output) {
+  set.seed(8)
+  x_data <- -80:80
+  errors <- rnorm(160,0,100)
   output$plot1 <- renderPlot({
-    set.seed(2016-05-25)
-    x_data <- -80:80
-    errors <- rnorm(160,0,100)
     y_data <- input$b0 + input$b1*x_data + input$b2*x_data^2 + input$b3*x_data^3 + errors
     ggplot(data.frame(x_data, y_data),aes(x=x_data,y=y_data)) + geom_point() + ylim(-1000,1000)
   })
   output$plot2 <- renderPlot({
-    set.seed(2016-05-25)
-    x_data <- -80:80
-    errors <- rnorm(160,0,100)
     y_data <- input$b0 + input$b1*x_data + input$b2*x_data^2 + input$b3*x_data^3 + errors
     fitted_model <- lm(y_data ~ x_data)
     modelresiduals <- fitted_model$residuals
