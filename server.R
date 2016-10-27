1c64ff910efa347f89ef029c1eba91759359e255
louiserfallon <louise.r.fallon@gmail.com>
2016-10-27 13:56:18 +0100

    Upadted with ggplots, residual plots, and better layout.

--- server.R
+++ server.R
@@ -1,23 +1,24 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+# Server application for quadratic residual plots
+# Submitted for the Shiny Assignment of the DDP course
+
 
 library(shiny)
-
+library(ggplot2)
 shinyServer(function(input, output) {
   output$plot1 <- renderPlot({
     set.seed(2016-05-25)
     x_data <- -80:80
     errors <- rnorm(160,0,100)
     y_data <- input$b0 + input$b1*x_data + input$b2*x_data^2 + input$b3*x_data^3 + errors
-    maxX <- input$sliderX[2]
-    minY <- input$sliderY[1]
-    maxY <- input$sliderY[2]
-    plot(x_data, y_data, xlim = c(-80, 80), ylim = c(-1000, 1000))
+    ggplot(data.frame(x_data, y_data),aes(x=x_data,y=y_data)) + geom_point() + ylim(-1000,1000)
+  })
+  output$plot2 <- renderPlot({
+    set.seed(2016-05-25)
+    x_data <- -80:80
+    errors <- rnorm(160,0,100)
+    y_data <- input$b0 + input$b1*x_data + input$b2*x_data^2 + input$b3*x_data^3 + errors
+    fitted_model <- lm(y_data ~ x_data)
+    modelresiduals <- fitted_model$residuals
+    ggplot(data.frame(x_data, modelresiduals),aes(x=x_data,y=modelresiduals)) + geom_point(color="darkblue") + ylim(-1000,1000)
   })
 })
--- ui.R
+++ ui.R
@@ -1,37 +1,33 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+# UI application for quadratic residual plots
+# Submitted for the Shiny Assignment of the DDP course
 
 library(shiny)
 
 shinyUI(fluidPage(
-  titlePanel("Plot Random Numbers"),
-  sidebarLayout(
-    sidebarPanel(
-      h2("Quadratic Impact on Residual Plots"),
-      h4("This application shows the different residual plots for
+  titlePanel("Quadratic Impact on Residual Plots"),
+    fixedRow(
+      column(4,
+      h6("This application shows the different residual plots for
          a linear model:"),
       h6("y= b1 + b2x"),
       h6("with the dependent variable y and the independent variable x,
          for different true models:"),
-      h6("y = b1 + b2x+ b3x^2+b3x^3"),
+      h6("y = b1 + b2x+ b3x^2+b3x^3")),
+      column(4,
       sliderInput("b0", "value for b0:",
                   min = -800, max = 800, value = 0, step= 0.1),
-      sliderInput("b1", "b1:",
-                  min = -80, max = 80, value = 0, step= 0.1),
       sliderInput("b2", "b2:",
-                  min = -0.8, max = 0.8, value = 0, step= 0.05),
+                  min = -0.8, max = 0.8, value = 0, step= 0.05)),
+      column(4,
+      sliderInput("b1", "b1:",
+                         min = -80, max = 80, value = 0, step= 0.1),
       sliderInput("b3", "b3:",
-                  min = -0.008, max = 0.008, value = 0, step= 0.001)
-    ),
-    mainPanel(
-      h3("Graph of raw data"),
-      plotOutput("plot1")
-    )
-  )
-))
+                  min = -0.008, max = 0.008, value = 0, step= 0.001))),
+  fixedRow(
+    column(6,
+           h2("Data Plot"),
+           plotOutput("plot1", height="300px")),
+    column(6,
+           h2("Residual Plot"),
+           plotOutput("plot2",height="300px")))
+  ))
